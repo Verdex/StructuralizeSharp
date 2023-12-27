569b97f7e4dfafa7550899d595f4c76b6b0058c3
David L. Inman <davidlevere@gmail.com>
2023-12-27 16:57:28 -0500

    matcher tests inc prog

---
+++ test/DataMatchTests/MatcherTests.cs
@@ -0,0 +1,25 @@
+
+using System.Linq;
+using System.Collections.Generic;
+
+using StructuralizeSharp.DataMatch;
+
+namespace StructuralizeSharpTests.DataMatcherTests;
+
+[TestFixture]
+public class MatcherTests {
+
+    public record NumberData(int Number) : IData;
+
+    [Test]
+    public void ShouldCaptureData() {
+        var output = new Matcher().Match(new Capture("x"), new NumberData(5)).ToList();
+
+        Assert.That(output.Count, Is.EqualTo(1));
+        Assert.That(output[0].Count, Is.EqualTo(1));
+
+        var match = output[0].ToDictionary();
+
+        Assert.That(match["x"], Is.EqualTo(new NumberData(5)));
+    }
+}
