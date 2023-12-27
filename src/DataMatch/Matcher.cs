05812504bc487613b4596210fb1ecf424044be53
David L. Inman <davidlevere@gmail.com>
2023-12-27 16:37:08 -0500

    capture impl

--- src/DataMatch/Matcher.cs
+++ src/DataMatch/Matcher.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 namespace StructuralizeSharp.DataMatch;
 
 public interface IMatcher {
-    IEnumerable<(string, IData)> Match(IPattern pattern, IData data);
+    IEnumerable<IEnumerable<(string, IData)>> Match(IPattern pattern, IData data);
 }
 
 
 public class Matcher : IMatcher {
-    
-
+    public IEnumerable<IEnumerable<(string, IData)>> Match(IPattern pattern, IData data) {
+        return (pattern, data) switch {
+            (Capture c, _) => new [] { new [] { (c.Name, data) } },
+            _ => throw new NotImplementedException(),
+        };
+    }
 }
